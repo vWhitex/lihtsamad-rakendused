f436fc2e83f6666b7387b77a5a0940da3068f481
Tammo Tammisto <Tammo.Tammisto@tptlive.ee>
2023-06-07 10:43:44 +0300

    commit

--- Quiz/Form1.cs
+++ Quiz/Form1.cs
@@ -140,7 +140,7 @@ namespace MathQuiz
                 // a MessageBox, and fill in the answers.
                 timer1.Stop();
                 timeLabel.Text = "Time's up!";
-                MessageBox.Show("You didn't finish in time.", "Sorry!");
+                MessageBox.Show("Whats 9+10?.", "You stupid!");
                 sum.Value = addend1 + addend2;
                 difference.Value = minuend - subtrahend;
                 product.Value = multiplicand * multiplier;
